019bdb112b9e51ea6be3543272c0c5ab42f62a3d
Toby Benjamin <toby@tobybenjamin.com>
2018-01-25 15:20:37 -0500

    prod update

--- site/web/app/themes/open-data-week-theme/MODAdata/newsletter_form.php
+++ site/web/app/themes/open-data-week-theme/MODAdata/newsletter_form.php
@@ -2,11 +2,13 @@
 function moda_newsletter_form( ) { ?>
 
 
+<!--
       <form id="newsletter" class="one_field">
         <input type="text" placeholder="enter your email address" />
         <input type="submit" value="Subscribe" />
       </form>
-
+-->
+	<a href="https://www.surveymonkey.com/r/NYCOpenDataEmailSignUp" class="button rounded" style="max-width: 280px;margin:12px auto;">Subscribe</a>
 
 <?php } // end function  
 ?>
