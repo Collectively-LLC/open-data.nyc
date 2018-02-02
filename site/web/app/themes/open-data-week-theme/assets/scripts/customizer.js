4c71a5261443e974961fba98ff635c5a2f75b310
Toby Benjamin <toby@tobybenjamin.com>
2018-02-02 12:08:55 -0500

    add javascript to modal URL

--- site/web/app/themes/open-data-week-theme/assets/scripts/customizer.js
+++ site/web/app/themes/open-data-week-theme/assets/scripts/customizer.js
@@ -6,3 +6,12 @@
     });
   });
 })(jQuery);
+
+// BEGIN Custom Modal URL JS
+// jQuery(document).ready(function(){
+//   // if there is a hash, it contains the string 'details', and there's actually a modal with that ID, show the modal
+//   if(window.location.hash.indexOf("details") >= 0 && jQuery(window.location.hash).length) {
+//     jQuery(window.location.hash).modal("show");
+//   }
+// });
+// END Custom Modal URL JS
--- site/web/app/themes/open-data-week-theme/templates/footer.php
+++ site/web/app/themes/open-data-week-theme/templates/footer.php
@@ -16,3 +16,16 @@
 </div>
 </div>
 </footer>
+
+
+<script>
+// BEGIN Custom Modal URL JS
+jQuery(document).ready(function(){
+  // if there is a hash, it contains the string 'details', and there's actually a modal with that ID, show the modal
+  if(window.location.hash.indexOf("details") >= 0 && jQuery(window.location.hash).length) {
+    jQuery(window.location.hash).modal("show");
+  }
+});
+// END Custom Modal URL JS
+</script>
+
