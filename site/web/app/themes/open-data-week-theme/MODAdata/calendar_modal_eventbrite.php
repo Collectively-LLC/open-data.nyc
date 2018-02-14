467a1381f639cdae4c18685b07b20dbce80c018a
Toby Benjamin <toby@tobybenjamin.com>
2018-02-14 16:39:03 -0500

    add unique IDs to Eb modals to fix same-Eb-event widget error

--- site/web/app/themes/open-data-week-theme/MODAdata/calendar_modal_eventbrite.php
+++ site/web/app/themes/open-data-week-theme/MODAdata/calendar_modal_eventbrite.php
@@ -11,6 +11,7 @@ $events = moda_get_items('events',array( 'date' => 'ASC'));
 		if(strpos( $allmeta[cmb_pre().'register'], 'eventbrite' ) > 0 ) {
 
 			$eb_event_id = eb_event_id($allmeta[cmb_pre().'register']);
+			$eb_container_id = $eb_event_id.'-'.$id;
 
 			echo '<div class="modal fade" id="register'.$id.'" tabindex="-1" role="dialog" aria-hidden="true">
 				  <div class="modal-dialog" role="document">
@@ -18,14 +19,14 @@ $events = moda_get_items('events',array( 'date' => 'ASC'));
 				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 				          <span aria-hidden="true">&times;</span>
 				        </button>
-				        <div id="eventbrite-widget-container-'.$eb_event_id.'" style="height: 0"></div>
+				        <div id="eventbrite-widget-container-'.$eb_container_id.'" style="height: 0"></div>
 				    </div>
 				  </div>
 				</div>
 
 				<script type="text/javascript">
 				function load_register'.$id.'() { 
-					if(jQuery(\'#eventbrite-widget-container-'.$eb_event_id.'\').html() == "") {
+					if(jQuery(\'#eventbrite-widget-container-'.$eb_container_id.'\').html() == "") {
 					    var exampleCallback = function() {
 					        console.log(\'Order complete!\');
 					    };
@@ -33,7 +34,7 @@ $events = moda_get_items('events',array( 'date' => 'ASC'));
 					        // Required
 					        widgetType: \'checkout\',
 					        eventId: \''.$eb_event_id.'\',
-					        iframeContainerId: \'eventbrite-widget-container-'.$eb_event_id.'\',
+					        iframeContainerId: \'eventbrite-widget-container-'.$eb_container_id.'\',
 					        // Optional
 					        iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
 					        onOrderComplete: exampleCallback  // Method called when an order has successfully completed
